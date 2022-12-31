e1af912200690766950635478c0f1bd904ecc163
Andy Ward.uk <andy.ward.uk@gmail.com>
2022-12-31 10:27:16 +0000

    Day 15 tidy

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -14,32 +14,25 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn part1(input: &[u64]) -> u64 {
-    let mut g1 = Generator::new(input[0], 16807);
-    let mut g2 = Generator::new(input[1], 48271);
+    let mut g1 = Generator::new(input[0], 16807, 0);
+    let mut g2 = Generator::new(input[1], 48271, 0);
 
-    let mut matches = 0;
-
-    for _ in 0..40_000_000 {
-        let v1 = g1.next();
-        let v2 = g2.next();
-
-        if v1 & 0xffff == v2 & 0xffff {
-            matches += 1;
-        }
-    }
-
-    matches
+    count_matches(&mut g1, &mut g2, 40_000_000)
 }
 
 fn part2(input: &[u64]) -> u64 {
-    let mut g1 = Generator::new(input[0], 16807);
-    let mut g2 = Generator::new(input[1], 48271);
+    let mut g1 = Generator::new(input[0], 16807, 0x03);
+    let mut g2 = Generator::new(input[1], 48271, 0x07);
 
+    count_matches(&mut g1, &mut g2, 5_000_000)
+}
+
+fn count_matches(g1: &mut Generator, g2: &mut Generator, iterations: usize) -> u64 {
     let mut matches = 0;
 
-    for _ in 0..5_000_000 {
-        let v1 = g1.next_mask(0x3);
-        let v2 = g2.next_mask(0x7);
+    for _ in 0..iterations {
+        let v1 = g1.next();
+        let v2 = g2.next();
 
         if v1 & 0xffff == v2 & 0xffff {
             matches += 1;
@@ -52,30 +45,35 @@ fn part2(input: &[u64]) -> u64 {
 struct Generator {
     last: u64,
     factor: u64,
+    mask: u64,
 }
 
 impl Generator {
-    fn new(init: u64, factor: u64) -> Self {
-        Generator { last: init, factor }
+    fn new(init: u64, factor: u64, mask: u64) -> Self {
+        Generator {
+            last: init,
+            factor,
+            mask,
+        }
     }
 
     fn next(&mut self) -> u64 {
-        let product = self.last * self.factor;
-        let next = product % 2_147_483_647;
-        self.last = next;
-        next
-    }
+        let mut num = self.last;
 
-    fn next_mask(&mut self, mask: u64) -> u64 {
         loop {
-            let next = self.next();
+            num = (num * self.factor) % 2_147_483_647;
 
-            if next & mask == 0 {
-                break next;
+            if num & self.mask == 0 {
+                break;
             }
         }
+
+        self.last = num;
+
+        num
     }
 }
+
 // Input parsing
 
 fn input_transform(line: String) -> u64 {
