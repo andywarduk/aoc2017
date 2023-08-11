7ff6998483774fd2114400f33a6f9562de8e31f6
Andy Ward.uk <andy.ward.uk@gmail.com>
2023-08-11 10:38:45 +0100

    Test fixes

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -256,7 +256,7 @@ rcv d
     fn test1() {
         let input = parse_test_vec(EXAMPLE1, input_transform).unwrap();
         assert_eq!(part1(&input), 4);
-        assert_eq!(part2(&input), 0 /* TODO */);
+        assert_eq!(part2(&input), 1);
     }
 
     #[test]
